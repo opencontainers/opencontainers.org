925e03a73564e397f8a955e9354e351ce434b90f
Celeste Horgan <celeste@cncf.io>
2020-04-29 18:08:46 -0700

    Cleanup assets/scripts a bit
    Signed-off-by: Celeste Horgan <celeste@cncf.io>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,31 @@
-const navbarBurger = () => {
-  const burger = $(".navbar-burger"),
-    menu = $(".navbar-menu");
+// const navbarBurger = () => {
+//   const burger = $(".navbar-burger"),
+//     menu = $(".navbar-menu");
+// 
+//   burger.click(() => {
+// 
+// 
+//     [burger, menu].forEach((el) => el.toggleClass('is-active'));
+//   });
+// }
+// 
+// $(() => {
+//   console.log("Welcome to the CNCF's Hugo + Netlify starter");
+// 
+//   navbarBurger();
+// });
 
-  burger.click(() => {
+import 'alpinejs'
 
 
-    [burger, menu].forEach((el) => el.toggleClass('is-active'));
-  });
+//Javascript to toggle the nav-menu
+document.getElementById('nav-open').onclick = function () {
+    document.getElementById("nav-menu").classList.toggle("hidden");
+    document.getElementById("nav-open").classList.toggle("hidden");
+    document.getElementById("nav-close").classList.toggle("hidden");
 }
-
-$(() => {
-  console.log("Welcome to the CNCF's Hugo + Netlify starter");
-
-  navbarBurger();
-});
+document.getElementById('nav-close').onclick = function () {
+    document.getElementById("nav-menu").classList.toggle("hidden");
+    document.getElementById("nav-close").classList.toggle("hidden");
+    document.getElementById("nav-open").classList.toggle("hidden");
+}
